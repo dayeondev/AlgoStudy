313592dd137dcdd8a160b6fc29151b54f7406b24
dayeondev <dayeon.dev@gmail.com>
2020-01-11 23:43:54 +0900

    200111

---
+++ (x)200111_n과m.c
@@ -0,0 +1,29 @@
+// https://www.acmicpc.net/problem/15650
+
+#include <stdio.h>
+
+// global
+int i, j;
+
+void sequence(n, m){
+    // for(i = 0; i < n; i++){
+    //     for(j = i; j < )
+    // }
+}
+
+
+int main(){
+    // 3, 1일경우
+    // 1부터 3까지
+    // 1개를 고른 수열
+    // 1
+    // 2
+    // 3
+
+    int i;
+    int n, m;
+
+
+
+    return 0;
+}
