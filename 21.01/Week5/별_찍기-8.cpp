e83c16e1aae2e750bc60accf6317013c7b9fc021
DayeonOh <dayeon.dev@gmail.com>
2021-01-25 23:40:37 +0900

    21/01/25

---
+++ 21.01/Week5/별_찍기-8.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  
+  int n;
+  cin >> n;
+
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j <= i; j++){
+      cout << '*';
+    }
+    for(int j = i + 1; j < n; j++){
+      cout << ' ';
+    }
+    for(int j = i + 1; j < n; j++){
+      cout << ' ';
+    }
+    for(int j = 0; j <= i; j++){
+      cout << '*';
+    }
+    cout << '\n';
+    
+  }
+
+  for(int i = n - 2; i >= 0; i--){
+
+    for(int j = 0; j <= i; j++){
+      cout << '*';
+    }
+    for(int j = i + 1; j < n; j++){
+      cout << ' ';
+    }
+    for(int j = i + 1; j < n; j++){
+      cout << ' ';
+    }
+    for(int j = 0; j <= i; j++){
+      cout << '*';
+    }
+    cout << '\n';
+  }
+
+
+  return 0;
+}
