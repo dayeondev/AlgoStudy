be99e88b7e7c4f5efdd4a59e1d7100c3f4aae0d3
Dayeon Oh <dayeon.dev@gmail.com>
2022-01-02 23:25:54 +0900

    22/01/02

---
+++ 22.01/Week2/README.md
@@ -0,0 +1,7 @@
+### 22.01 Week2
+
+-------
+
+#### 2(일)
+
+1. [숫자의 합](https://www.acmicpc.net/problem/11720)
---
+++ 22.01/Week2/숫자의_합.py
@@ -0,0 +1,7 @@
+length = int(input())
+nums = list(map(int,input()))
+result = 0
+for num in nums:
+    result += num
+
+print(result)
