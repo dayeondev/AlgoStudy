0f6f8fec89f11a98670c23d7c620f97409bb7ef9
Dayeon Oh <dayeon.dev@gmail.com>
2022-06-25 21:30:36 +0900

    22.06.25.

---
+++ etc/15680_연세대학교.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main(void){
+   int n;
+   cin >> n;
+   string result = n==0?"YONSEI":"Leading the Way to the Future";
+   cout << result << endl;
+}
+
