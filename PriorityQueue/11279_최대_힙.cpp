ba702631748c28ddd3a2a3aa1c027da6ed76a1e7
dayeon <dayeon.dev@gmail.com>
2022-07-03 22:49:58 +0900

    22.07.03.

---
+++ PriorityQueue/11279_최대_힙.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#include<queue>
+
+using namespace std;
+
+int n;
+priority_queue<int, vector<int>, less<int>> q;
+
+int main(void){
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+
+    cin >>n;
+    int tmp;
+    for(int i = 0; i < n; i++){
+        cin >> tmp;
+        if(tmp==0){
+            if(q.empty()){
+                cout << "0\n";
+            }
+            else{
+                cout << q.top() << '\n';
+                q.pop();
+            }
+        }
+        else{
+            q.push(tmp);
+        }
+    }
+}
