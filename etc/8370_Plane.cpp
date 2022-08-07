dc0880bcc30d524ee6026df62d5cc21f8013be32
dayeon <dayeon.dev@gmail.com>
2022-08-07 23:01:59 +0900

    22.08.07.

---
+++ etc/8370_Plane.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int n1, k1, n2, k2;
+int main(void){
+    int n1, k1, n2, k2;
+    cin >> n1 >> k1 >> n2 >> k2;
+    cout << n1*k1+n2*k2 << '\n';
+}
