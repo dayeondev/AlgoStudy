6df054c5ddf6c4b6634c1175c59f1cd284102896
dayeon <dayeon.dev@gmail.com>
2022-08-12 22:33:19 +0900

    22.08.12.

---
+++ etc/15963_CASIO.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+string n, m;
+
+int main(void){
+    cin >> n >> m;
+
+    if(n == m){
+        cout << "1\n";
+    }
+    else{
+        cout << "0\n";
+    }
+
+}
