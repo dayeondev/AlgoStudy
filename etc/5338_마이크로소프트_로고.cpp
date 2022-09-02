70126fa4620801bd3a4fd07e33647031034ee242
dayeon <dayeon.dev@gmail.com>
2022-09-02 23:39:30 +0900

    22.09.02.

---
+++ etc/5338_마이크로소프트_로고.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(void){
+    cout << "       _.-;;-._\n";
+    cout << "'-..-'|   ||   |\n";
+    cout << "'-..-'|_.-;;-._|\n";
+    cout << "'-..-'|   ||   |\n";
+    cout << "'-..-'|_.-''-._|\n";
+}
