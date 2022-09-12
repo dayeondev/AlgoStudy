72f877401ebb25b22a2d159dd8e8a9f2ac43ed8e
dayeon <dayeon.dev@gmail.com>
2022-09-12 21:07:59 +0900

    22.09.12.

---
+++ Backtracking/15654_N과_M_5.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define MAX 8
+
+using namespace std;
+
+int n, m;
+int arr[MAX];
+bool visited[MAX];
+vector<int> v;
+
+void DFS(int depth){
+    if (depth == m){
+        for(int i = 0; i < v.size(); i++){
+            cout << v[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for(int i = 0; i < n; i++){
+        if(visited[i]){
+            // pass
+        }
+        else{
+            visited[i] = true;
+            v.push_back(arr[i]);
+            DFS(depth+1);
+            visited[i] = false;
+            v.pop_back();
+        }
+    }
+}
+
+int main(void){
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    sort(arr, arr+n);
+
+    DFS(0);
+    
+}
