683eb28efecc6200a7c18a4bef768c1d88fc759e
dayeon <dayeon.dev@gmail.com>
2022-10-12 23:05:48 +0900

    22.10.12.

---
+++ etc/9316_Hello_Judge.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n;
+
+int main(void){
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cout << "Hello World, Judge " << i+1 << "!\n";
+    }
+}
