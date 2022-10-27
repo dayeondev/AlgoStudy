e6be227910bec289704519376010bfdc9c622211
dayeon <dayeon.dev@gmail.com>
2022-10-27 23:55:42 +0900

    22.10.27.

---
+++ etc/5717_상근이의_친구들.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int n1, n2;
+
+int main(void){
+    cin >> n1 >> n2;
+    while (n1!=0&&n2!=0)
+    {
+        cout << n1+n2 << '\n';
+
+        cin >> n1 >> n2;
+    }
+    
+}
